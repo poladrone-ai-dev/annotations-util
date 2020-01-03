28fe9c65f31fe3d27512bb7f7c8357c441e5d4f3
Brian Sia <briansia93@gmail.com>
2020-01-03 11:29:47 +0800

    added argparser to change_to_darknet_format. added kmeans.py to calculate anchor boxes

--- scripts/change_to_darknet_format.py
+++ scripts/change_to_darknet_format.py
@@ -2,24 +2,30 @@ import os
 import fileinput
 import sys
 import random
+import argparse
 
-filepath = r'D:\testing_data\things_to_annotate_jpg\combined\combined\result\bb_box.txt'
-label_path = r'D:\PyTorch-YOLOv3-master\data\custom\labels'
-image_path = r'D:\PyTorch-YOLOv3-master\data\custom\images'
-filename_file = r'D:\PyTorch-YOLOv3-master\data\custom\filename.txt'
-valid_file = r'D:\PyTorch-YOLOv3-master\data\custom\valid.txt'
+parser = argparse.ArgumentParser()
+parser.add_argument("--annotation", type=str, required=True, help="input text file with name and bbox")
+parser.add_argument("--output", type=str, required=True, help="output directory for conversion")
+opt = parser.parse_args()
+
+filepath = opt.annotation
+label_path = os.path.join(opt.output, "labels")
+image_path = os.path.join(opt.output, "images")
+filename_file = os.path.join(opt.output, "filename.txt")
+valid_file = os.path.join(opt.output, "valid.txt")
 
 classes = ['0', '1', '2', '3'] # fill this with your class names
 WIDTH = 500
 HEIGHT = 500
 current_file = r''
 
-palm0_count = 0
-palm1_count = 0
-palm2_count = 0
-palm3_count = 0
+# palm0_count = 0
+# palm1_count = 0
+# palm2_count = 0
+# palm3_count = 0
 
-def split_file(file,out1,out2,percentage=0.8,isShuffle=True,seed=123):
+def split_file(file, out1, out2, percentage=0.7, isShuffle=True, seed=123):
     """Splits a file in 2 given the `percentage` to go in the large file."""
     random.seed(seed)
     with open(file, 'r', encoding="utf-8") as fin, \
@@ -51,18 +57,18 @@ with open(filepath, 'r') as fp:
         # multiclass training
         class_index = int(classes.index(values[4]))
 
-        if class_index == 0:
-            palm0_count += 1
-
-        if class_index == 1:
-            palm1_count += 1
-
-        if class_index == 2:
-            palm2_count += 1
-
-        if class_index == 3:
-            palm3_count += 1
-
+        # if class_index == 0:
+        #     palm0_count += 1
+        #
+        # if class_index == 1:
+        #     palm1_count += 1
+        #
+        # if class_index == 2:
+        #     palm2_count += 1
+        #
+        # if class_index == 3:
+        #     palm3_count += 1
+        print("Current file: " + current_file)
         anno = str(class_index) + " " + str(x_center) + " " + str(y_center) + " " + str(box_width) + " " + str(box_height) + '\n'
 
         with open(filename_file, 'a') as train_fp:
@@ -74,13 +80,13 @@ with open(filepath, 'r') as fp:
         with open(current_file, 'a') as out_fp:
             out_fp.write(anno)
 
-split_file(r'D:\PyTorch-YOLOv3-master\data\custom\filename.txt',
-           r'D:\PyTorch-YOLOv3-master\data\custom\train.txt',
-           r'D:\PyTorch-YOLOv3-master\data\custom\valid.txt')
+split_file(os.path.join(opt.output, "filename.txt"),
+           os.path.join(opt.output, "train.txt"),
+           os.path.join(opt.output, "valid.txt"))
 
 os.remove(filename_file)
-
-print("Palm0 count: " + str(palm0_count))
-print("Palm1 count: " + str(palm1_count))
-print("Palm2 count: " + str(palm2_count))
-print("Palm3 count: " + str(palm3_count))
+#
+# print("Palm0 count: " + str(palm0_count))
+# print("Palm1 count: " + str(palm1_count))
+# print("Palm2 count: " + str(palm2_count))
+# print("Palm3 count: " + str(palm3_count))
