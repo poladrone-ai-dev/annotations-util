2620d7f0e3fa08f9b3d2474e2dc7bc2764da6539
Brian Sia <briansia93@gmail.com>
2020-01-07 14:12:59 +0800

    data_augmentation returns full classname. changed the formula for train-valid split (WIP)

--- scripts/change_to_darknet_format.py
+++ scripts/change_to_darknet_format.py
@@ -4,6 +4,7 @@ import sys
 import random
 import argparse
 import shutil
+from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", type=str, required=True, help="input text file with name and bbox")
@@ -11,8 +12,6 @@ parser.add_argument("--output", type=str, required=True, help="output directory 
 parser.add_argument("--train", type=float, required=True, help="training data split (0-1)")
 parser.add_argument("--test", type=float, required=True, help="test data split (0-1)")
 parser.add_argument("--valid", type=float, required=True, help="valid data split (0-1)")
-parser.add_argument("--width", type=int, required=True, help="input image width")
-parser.add_argument("--height", type=int, required=True, help="input image height")
 parser.add_argument("--classes", type=str, required=True, help="text file with class names")
 opt = parser.parse_args()
 
@@ -21,8 +20,6 @@ filepath = opt.input
 filename_file = os.path.join(opt.output, "filename.txt")
 
 classes = [] # fill this with your class names
-WIDTH = opt.width
-HEIGHT = opt.height
 current_file = r''
 
 def move_files():
@@ -70,6 +67,12 @@ def move_files():
             label_dest = os.path.join(opt.output, "test", os.path.basename(label_source))
             shutil.copyfile(label_source, label_dest)
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 def split_test_data(file):
     num_data = sum(1 for line in open(file))
     print(num_data)
@@ -85,7 +88,7 @@ def split_test_data(file):
 
     all_data_no_augment = [data for data in all_data if "-f0." not in data and "-f1." not in data]
 
-    while len(test_data) < int(opt.test * num_data):
+    while len(test_data) <= int(opt.test * num_data):
         filename = all_data_no_augment[random.randrange(len(all_data_no_augment)) ]
         filename_no_ext = filename[:-4]
         filename_vflip = filename_no_ext + "-f0.jpg"
@@ -117,10 +120,18 @@ def split_test_data(file):
         for line in test_data:
             test_file.write(line + "\n")
 
-def split_file(file, out1, out2, seed=123):
+    train_valid_len = file_len(os.path.join(opt.output, "train_valid.txt"))
+    print(train_valid_len)
+
+    return [len(all_data) , train_valid_len]
+
+def split_file(file, out1, out2, file_count, seed=123):
     """Splits a file in 2 given the `percentage` to go in the large file."""
     random.seed(seed)
-    percentage = (1 - opt.test) * opt.train
+    all_count = file_count[0]
+    train_valid_count = file_count[1]
+
+    percentage = (all_count * opt.train) / train_valid_count
 
     with open(file, 'r', encoding="utf-8") as fin, \
             open(out1, 'w') as foutBig, \
@@ -159,11 +170,18 @@ if __name__ == "__main__":
 
     with open(filepath, 'r') as fp:
         for line in fp:
-            filename = line.split(' ')[0][:-4]
+            filename_no_ext = line.split(' ')[0][:-4]
+            image_filename = line.split(" ")[0]
             values = line.split(' ')[1].split(',')
             values[-1] = values[-1].replace('\n', '')
-            current_file = os.path.join(opt.output, filename + '.txt')
+            current_file = os.path.join(opt.output, filename_no_ext + '.txt')
             image_file = line.split(' ')[0]
+
+            try:
+                im = Image.open(image_filename)
+                WIDTH, HEIGHT = im.size
+            except:
+                print("Could not find " + image_filename + ". Please check that image exists.")
 
             x_center = (int(values[2]) + int(values[0])) / (2 * WIDTH)
             y_center = (int(values[3]) + int(values[1])) / (2 * HEIGHT)
@@ -177,9 +195,9 @@ if __name__ == "__main__":
             # anno = str(class_index) + " " + str(int(values[0])) + " " + str(int(values[1])) + " " + str(int(values[2])) + " " + \
             #        str(int(values[3])) + '\n'
 
-            print("output: " + opt.output)
-            print("current file: " + current_file)
-
+            # print("output: " + opt.output)
+            # print("current file: " + current_file)
+            print(image_file)
             with open(filename_file, 'a') as train_fp:
                 if image_file not in seen_lines:
                     train_fp.write(image_file)
@@ -189,14 +207,15 @@ if __name__ == "__main__":
             with open(current_file, 'a') as out_fp:
                 out_fp.write(anno)
 
-    split_test_data(os.path.join(opt.output, "filename.txt"))
+    [all_count, train_valid_count] = split_test_data(os.path.join(opt.output, filename_file))
 
     split_file(os.path.join(opt.output, "train_valid.txt"),
                os.path.join(opt.output, "train.txt"),
-               os.path.join(opt.output, "valid.txt")
+               os.path.join(opt.output, "valid.txt"),
+               [all_count, train_valid_count]
     )
 
-    os.remove(filename_file)
-    os.remove(os.path.join(opt.output, "train_valid.txt"))
+    # os.remove(filename_file)
+    # os.remove(os.path.join(opt.output, "train_valid.txt"))
 
     move_files()
--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -55,7 +55,7 @@ def data_augmentation(input_path, output_path):
             element_filename = element.find('filename').text
             base_filename = os.path.join(input_path, element_filename)                              
             img = cv2.imread(base_filename)
-            rows, cols = img.shape[:2] 
+            rows, cols = img.shape[:2]
 
 
             img_split = element_filename.strip().split('.jpg')
@@ -73,7 +73,7 @@ def data_augmentation(input_path, output_path):
                 # copying original images to new path
                 new_name = img_split[0] + '.jpg'
                 new_path = os.path.join(output_path, new_name) # join with augmented image path
-                lines = [new_path, ' ', str(x1), ',', str(y1), ',', str(x2), ',', str(y2), ',', class_name[-1], '\n']
+                lines = [new_path, ' ', str(x1), ',', str(y1), ',', str(x2), ',', str(y2), ',', class_name, '\n']
                 pwd_lines.append(lines)
                 if not os.path.isfile(new_path):
                     cv2.imwrite(new_path, img)
@@ -99,7 +99,7 @@ def data_augmentation(input_path, output_path):
 
                     new_name = img_split[0] + '-' + f_str + '.jpg'
                     new_path = os.path.join(output_path, new_name)
-                    lines = [new_path, ' ', str(f_x1), ',', str(f_y1), ',', str(f_x2), ',', str(f_y2), ',', class_name[-1], '\n']
+                    lines = [new_path, ' ', str(f_x1), ',', str(f_y1), ',', str(f_x2), ',', str(f_y2), ',', class_name, '\n']
                     pwd_lines.append(lines)
                     if not os.path.isfile(new_path):
                         cv2.imwrite(new_path, f_img)
